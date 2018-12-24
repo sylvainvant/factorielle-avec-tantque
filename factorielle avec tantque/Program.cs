8877f83f41dab3e64b0fb65cf06d67b505ea9e54
sylvainvant <sylvainvantieghem@gmail.com>
2018-12-24 15:16:57 +0100

    first commit

---
+++ factorielle avec tantque/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorielle_avec_tantque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // N est l'égale du compteur
+         double N=0;
+         string saissie;
+         double resultat=1;
+         //etapes:
+            Console.WriteLine("entrez la valeur n a calculer:");
+        
+            saissie = Console.ReadLine();
+            N = Convert.ToDouble(saissie);
+           
+
+
+            if (N==0 | N==1)
+            {
+                Console.WriteLine("La factorielle de "+N+" avec tantque est :" + resultat);
+            }
+                else
+                {
+
+                while (N > 1) 
+                    {
+                    resultat = (resultat * N);
+                    N = N - 1;
+                    }
+                Console.WriteLine("La factorielle de " + N + " avec tantque est :" + resultat);
+
+            }
+            // permet de conserver affichage consol
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
